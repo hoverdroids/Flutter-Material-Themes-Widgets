e89bb645232328f3cb1c1b819964360e8f178303
Chris Sprague <spragucm@gmail.com>
2023-12-16 12:38:56 -0800

    WIP - deleted all but .git, .dart_tool, etc

--- lib/drawers/tab_drawer.dart
+++
@@ -1,4 +0,0 @@
-/*
-import 'package:flutter/material.dart';
-import 'package:material_themes_manager/material_themes_manager.dart';
-import 'package:provider/provider.dart';*/
--- lib/material_themes_widgets.dart
+++
@@ -1,7 +0,0 @@
-library material_themes_widgets;
-
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
