88ce8ebd4ac0b2c34b35d2b33e672d85a298d842
Chris Sprague <spragucm@gmail.com>
2023-12-19 13:56:25 -0800

    WIP - Added Collection Utils with toUniqueList and toTrimmedUniqueList (#8)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,4 @@
+## [1.10.0] - Added Collection Utils with toUniqueList and toTrimmedUniqueList
 ## [1.9.0]  - Added TextEntryDialog
 ## [1.8.0]  - Added card fields to card items
 ## [1.7.2]  - Using Material Themes Manager 1.4.0
---
+++ lib/utils/collection_utils.dart
@@ -0,0 +1,16 @@
+extension ListStringUtils on List<String> {
+  List<String> toTrimmedUniqueList() => (this as Iterable<String>).toTrimmedUniqueList() as List<String>;
+
+  List<String> trimAll() => (this as Iterable<String>).trimAll() as List<String>;
+
+  List<String> toUniqueList({bool growable = true}) => (this as Iterable<String>).toUniqueList(growable: growable) as List<String>;
+}
+
+extension IterableStringUtils on Iterable<String> {
+
+  Iterable<String> toTrimmedUniqueList() => trimAll().toUniqueList();
+
+  Iterable<String> trimAll() => map((string) => string.replaceAllMapped(RegExp(r'^\s+|\s+$'), (match) => ""));
+
+  Iterable<String> toUniqueList({bool growable = true}) => toSet().toList(growable:growable);
+}
